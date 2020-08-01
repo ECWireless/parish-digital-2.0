c0e6110f4d43870c1db3cf76acafdd0cdf026b56
ECWireless <Elliott@valtechcreative.com>
2020-08-01 15:25:00 -0600

    Reconfigure plugin

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,16 +1,19 @@
-import React from 'react'
-import NextDocument from 'next/document'
+import Document from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
-export default class Document extends NextDocument {
-  static async getInitialProps (ctx) {
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet()
     const originalRenderPage = ctx.renderPage
+
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
         })
-      const initialProps = await NextDocument.getInitialProps(ctx)
+
+      const initialProps = await Document.getInitialProps(ctx)
       return {
         ...initialProps,
         styles: (
@@ -18,7 +21,7 @@ export default class Document extends NextDocument {
             {initialProps.styles}
             {sheet.getStyleElement()}
           </>
-        )
+        ),
       }
     } finally {
       sheet.seal()
