735c5a4dd5ac615240cc177f2e9e5704d3f0bfdd
ECWireless <Elliott@valtechcreative.com>
2020-08-13 17:32:12 -0600

    JWT Variable

--- frontend/pages/api/login.js
+++ frontend/pages/api/login.js
@@ -43,5 +43,5 @@ const createSessions = () => {
 const signToken = () => {
     const jwt = require('jsonwebtoken');
     const jwtPayload = { name: 'test' };
-    return Promise.resolve(jwt.sign(jwtPayload, process.env.JTW_SECRET, { expiresIn: '2 days'}))
+    return Promise.resolve(jwt.sign(jwtPayload, 'secret', { expiresIn: '2 days'}))
 }
