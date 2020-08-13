ba4fb2c34e0b3182c6ede97d0853d28a9ad90a02
ECWireless <Elliott@valtechcreative.com>
2020-08-13 17:01:12 -0600

    Add config variables

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,9 @@
 module.exports = {
     env: {
         ENVIRONMENT_TEST: process.env.ENVIRONMENT_TEST,
+        SANITY_TOKEN: process.env.SANITY_TOKEN,
+        JWT_SECRET: process.env.JTW_SECRET,
+        JWT_PAYLOAD: process.env.JWT_PAYLOAD,
+        SANITY_DOC_ID: process.env.SANITY_DOC_ID,
     },
 }
