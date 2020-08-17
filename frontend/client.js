3b8034d628b7c22a74ab1570af005ee8e4c5fda3
ECWireless <Elliott@valtechcreative.com>
2020-08-17 09:55:02 -0600

    Add cdn

--- frontend/client.js
+++ frontend/client.js
@@ -5,5 +5,5 @@ export default sanityClient({
   projectId: 'w1dauhhn', // you can find this in sanity.json
   dataset: 'production', // or the name you chose in step 1
   token: process.env.SANITY_TOKEN,
-  useCdn: false // `false` if you want to ensure fresh data
+  useCdn: true // `false` if you want to ensure fresh data
 })
