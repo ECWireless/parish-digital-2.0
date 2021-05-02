90ed788eff6a60f023f2a35dcc1ea6e7808eee7a
ECWireless <Elliott@valtechcreative.com>
2021-05-02 14:21:51 -0600

    Change endpoint

--- frontend/components/Login/AuthFalse.js
+++ frontend/components/Login/AuthFalse.js
@@ -20,7 +20,7 @@ const AuthFalse = ({
 
   useEffect(() => {
     setLoading(true);
-    fetch('http://localhost:3000/api/auth', {
+    fetch('http://parishdigital.com/api/auth', {
       method: 'put',
       headers: {
         'Content-Type': 'application/json',
@@ -47,7 +47,7 @@ const AuthFalse = ({
     e.preventDefault();
     setLoading(true);
 
-    fetch('http://localhost:3000/api/login', {
+    fetch('http://parishdigital.com/api/login', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -62,7 +62,7 @@ const AuthFalse = ({
         setLoading(false);
       } else {
         window.sessionStorage.setItem('token', resp.token);
-        fetch('http://localhost:3000/api/auth', {
+        fetch('http://parishdigital.com/api/auth', {
           method: 'put',
           headers: {
               'Content-Type': 'application/json',
