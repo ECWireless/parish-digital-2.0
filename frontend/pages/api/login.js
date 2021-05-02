359ef5a3bff8604aeb67f2e33dd6aabf740ba62a
ECWireless <Elliott@valtechcreative.com>
2021-05-02 16:46:25 -0600

    Add staging to endpoint

--- frontend/pages/api/login.js
+++ frontend/pages/api/login.js
@@ -2,37 +2,37 @@ import client from '../../client'
 import groq from 'groq'
 
 const query = groq`*[_type == "login" && slug.current == "v1"][0]{
-    loginPassword,
+  loginPassword,
 }`
 
 export default async (req, res) => {
-    const bcrypt = require('bcrypt');
-    // const saltRounds = 10;
-    // bcrypt.hash(plainPassword, saltRounds, function(err, hash) {
-    // });
+  const bcrypt = require('bcrypt');
+  // const saltRounds = 10;
+  // bcrypt.hash(plainPassword, saltRounds, function(err, hash) {
+  // });
 
-    const plainPassword = req.body.password;
-    const queryObject = await client.fetch(query)
-    const hash = await queryObject.loginPassword
-    const match = await bcrypt.compare(plainPassword, hash);
+  const plainPassword = req.body.password;
+  const queryObject = await client.fetch(query)
+  const hash = await queryObject.loginPassword
+  const match = await bcrypt.compare(plainPassword, hash);
 
-    if (match) {
-        return createSessions()
-        .then(token => {
-            client.patch(process.env.SANITY_DOC_ID).set({loginToken: token}).commit()
-            .then(doc => {
-                return Promise.resolve(res.status(200).json({ password: 'logged in! Token:', token: doc.loginToken }))
-            })
-            .catch(err => {
-                console.error('Oh no, the update failed: ', err.message)
-            })
-        })
-        .catch(err => {
-            console.error('Wrong Password!')
-        })
-    } else {
-        return res.status(200).json({ password: 'wrong password!' })
-    }
+  if (match) {
+    return createSessions()
+    .then(token => {
+      client.patch(process.env.SANITY_DOC_ID).set({loginToken: token}).commit()
+      .then(doc => {
+        return Promise.resolve(res.status(200).json({ password: 'logged in! Token:', token: doc.loginToken }))
+      })
+      .catch(err => {
+        console.error('Oh no, the update failed: ', err.message)
+      })
+    })
+    .catch(err => {
+        console.error('Wrong Password!')
+    })
+  } else {
+    return res.status(200).json({ password: 'wrong password!' })
+  }
 }
 
 const createSessions = () => {
