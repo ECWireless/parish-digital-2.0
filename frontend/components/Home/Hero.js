71a1c6ff3af5610aeef3a890b601784b923b298b
ECWireless <Elliott@valtechcreative.com>
2021-10-20 18:02:47 -0600

    Add new web hero

--- frontend/components/Home/Hero.js
+++ frontend/components/Home/Hero.js
@@ -21,7 +21,7 @@ const Hero = ({
         <WebHero>
             {/* <HeroPhoto style={{ backgroundImage: `url(${heroImage})`}} /> */}
             <HeroVideo autoPlay loop muted playsInline>
-                <source src='/PD_Hero_Video_3.mp4' type="video/mp4" />
+                <source src='/PD_Hero_Video_4.mp4' type="video/mp4" />
             </HeroVideo>
             <HeroContainer>
                 <TexContainer>
