df3589f1e9f3ba9769767289dede182e40bcf7dd
ECWireless <Elliott@valtechcreative.com>
2021-12-07 14:27:44 -0700

    Change staging backend projectId

--- frontend/client.js
+++ frontend/client.js
@@ -2,7 +2,8 @@
 import sanityClient from '@sanity/client'
 
 export default sanityClient({
-  projectId: 'w1dauhhn', // you can find this in sanity.json
+  // projectId: 'w1dauhhn', // Production
+  projectId: 'pysf6rl6', // Staging
   dataset: 'production', // or the name you chose in step 1
   token: process.env.SANITY_TOKEN,
   useCdn: false, // `false` if you want to ensure fresh data
