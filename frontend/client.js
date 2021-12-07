5169feb9ceea5f8c4b58aafd1381ecd88c2a2084
ECWireless <Elliott@valtechcreative.com>
2021-12-07 14:40:03 -0700

    Add token correctly...

--- frontend/client.js
+++ frontend/client.js
@@ -8,7 +8,7 @@ export default sanityClient({
   // projectId: 'w1dauhhn', // Production
   projectId: 'pysf6rl6', // Staging
   dataset: 'production', // or the name you chose in step 1
-  token: process.env.SANITY_TOKEN,
+  token,
   useCdn: false, // `false` if you want to ensure fresh data
   ignoreBrowserTokenWarning: true,
 })
