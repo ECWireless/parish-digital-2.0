50d2a8e5e5924f17f51aa1919d94c49432c8eb90
ECWireless <Elliott@valtechcreative.com>
2021-12-21 09:16:42 -0700

    Update Description for Services

--- frontend/components/Service/Description.js
+++ frontend/components/Service/Description.js
@@ -1,150 +1,132 @@
 import Fade from 'react-reveal/Fade';
-import styled, { css } from 'styled-components'
+import Link from 'next/link'
+import styled from 'styled-components'
 import respondTo from '../Breakpoints'
 
-// Components
-import { colors, shadows } from '../theme'
-import { Box1, Box3 } from '../Boxes'
-import { Button2 } from '../Buttons'
-import { Card1 } from '../Cards'
+import { Box3 } from '../Boxes'
+import { Button3 } from '../Buttons'
+import { Card3 } from '../Cards'
 import { Container, Flex } from '../Containers'
-import { Line } from '../Lines'
-import { H3, H4, P3 } from '../Typography'
+import { H3, H4, P5, P6 } from '../Typography'
 
 const Description = ({
-    setModal,
-    descriptionHeading,
-    descriptionSubheading,
-    descriptionVideoLink,
-    descriptionParagraph1,
-    descriptionParagraph2,
-    descriptionButtonText,
+  heading,
+  subheading,
+  descriptionPhoto,
+  descriptionHeading,
+  descriptionParagraph1,
+  descriptionParagraph2,
 }) => {
-    return (
-        <Container>
-            <Box1 marginTop={75} marginBottom={25}>
-                <H4 uppercase center>
-                    {descriptionSubheading}
-                </H4>
-            </Box1>
-            <Box1 marginTop={25} marginBottom={75}>
-                <Flex align={'center'} justify={'space-around'}>
-                    <Fade ssrFadeout>
-                        <CustomLine style={{margin: 0}} height={5} width={150} color={colors.yellow} />
-                    </Fade>
-                    <H3 uppercase center>
-                        {descriptionHeading}
-                    </H3>
-                    <Fade ssrFadeout>
-                        <CustomLine style={{margin: 0}} height={5} width={150} color={colors.yellow} />
-                    </Fade>
-                </Flex>
-            </Box1>
-            <Fade bottom ssrFadeout>
-                <Box1 marginTop={50} marginBottom={50}>
-                    <Card1>
-                        <Flex style={{width: '100%'}} align={'center'} justify={'space-between'} respondFlip>
-                            <Video
-                                title="Customer Success Stories Example"
-                                src={descriptionVideoLink}
-                                frameBorder={0} allow={'autoplay; fullscreen'} allowFullScreen
-                            />
-                            <Flex align={'center'} justify={'cetner'} direction={'column'}>
-                                <Fade delay={200} bottom ssrFadeout>
-                                    <Box3 width={480}>
-                                        <P3>
-                                            {descriptionParagraph1}
-                                        </P3>
-                                    </Box3>
-                                </Fade>
-                                <Fade delay={400} bottom ssrFadeout>
-                                    <Box3  marginTop={25} width={480}>
-                                        <P3>
-                                            {descriptionParagraph2}
-                                        </P3>
-                                    </Box3>
-                                </Fade>
-                                <ButtonBox marginTop={50} width={480}>
-                                    <Button2 onClick={() => setModal(true)}>
-                                        {descriptionButtonText}
-                                    </Button2>
-                                </ButtonBox>
-                            </Flex>
-                        </Flex>
-                    </Card1>
-                </Box1>
-            </Fade>
-        </Container>
-    )
+  return (
+    <Container>
+      <StyledTextContainer1 align={'center'} direction={'column'} justify={'center'}>
+        <Box3 marginTop={50} marginBottom={25}>
+          <H3 align={'center'} uppercase weight={900}>{heading}</H3>
+        </Box3>
+        <Box3 marginBottom={25}>
+          <P6 style={{ fontStyle: 'italic' }} align={'center'}>{subheading}</P6>
+        </Box3>
+      </StyledTextContainer1>
+      <Box3 marginTop={50} marginBottom={50}>
+        <Fade bottom ssrFadeout>
+          <Card3>
+            <Flex justify={'space-between'} respondFlip>
+              <StyledPhoto style={{ backgroundImage: `url(${descriptionPhoto})`}} />
+              <Flex align={'center'} justify={'flex-start'} direction={'column'}>
+                <StyledTextContainer2>
+                  <Box3 marginBottom={25}>
+                    <H4 center style={{fontWeight: 900}} uppercase>
+                      {descriptionHeading}
+                    </H4>
+                  </Box3>
+                  <Fade delay={200} bottom ssrFadeout>
+                    <Box3 width={500} marginBottom={25}>
+                      <P5>
+                        {descriptionParagraph1}
+                      </P5>
+                    </Box3>
+                  </Fade>
+                  <Fade delay={400} bottom ssrFadeout>
+                    <Box3 width={500}>
+                      <P5>
+                        {descriptionParagraph2}
+                      </P5>
+                    </Box3>
+                  </Fade>
+                  <Box3 marginTop={32}>
+                    <Link href='/contact'>
+                      <Button3>
+                        Ready for a quote!
+                      </Button3>
+                    </Link>
+                  </Box3>
+                </StyledTextContainer2>
+              </Flex>
+            </Flex>
+          </Card3>
+        </Fade>
+      </Box3>
+    </Container>
+  )
 }
 
-export default Description
+export default Description;
 
-const CustomLine =  styled(Line)`
-    ${props => props.width && css`
-        width: ${props.width * .3}px;
+const StyledTextContainer1 = styled(Flex)`
+  margin: 0 auto;
+  width: 100%;
 
-        ${respondTo.xs`
-            width: ${props.width * .2}px;
-        `}
+  ${respondTo.xs`
+    width: 400px;
+  `}
 
-        ${respondTo.sm`
-            width: ${props.width * .5}px;
-        `}
+  ${respondTo.sm`
+    width: 600px;
+  `}
 
-        ${respondTo.md`
-            width: ${props.width * .6}px;
-        `}
+  ${respondTo.md`
+    width: 750px;
+  `}
+`;
 
-        ${respondTo.lg`
-            width: ${props.width}px;
-        `}
+const StyledTextContainer2 = styled.div`
+  box-sizing: border-box;
+  padding: 40px 20px;
 
-        ${respondTo.xl`
-            width: ${props.width * 2}px;
-        `}
-    `}
-`
+  ${respondTo.xs`
+    padding: 30px;
+  `}
 
-const Video = styled.iframe`
-    width: 25rem;
-    height: 15rem;
-    box-shadow: ${shadows.card};
-    margin-top: 5rem;
+  ${respondTo.sm`
+    padding: 40px;
+  `}
 
-    ${respondTo.xs`
-        width: 32rem;
-        height: 18rem;
-    `}
+  ${respondTo.md`
+    padding: 80px 40px;
+  `}
 
-    ${respondTo.sm`
-        width: 44rem;
-        height: 25rem;
-    `}
+  ${respondTo.xl`
+    padding: 180px 40px;
+  `}
+`;
 
-    ${respondTo.md`
-        width: 62rem;
-        height: 35rem;
-    `}
+const StyledPhoto = styled.div`
+  background-position: center;
+  background-size: cover;
+  height: 320px;
+  width: 100%;
 
-    ${respondTo.lg`
-        width: 60rem;
-        height: 34rem;
-        margin-top: 0;
-    `}
+  ${respondTo.md`
+    height: 420px;
+  `}
 
-    ${respondTo.xl`
-        width: 90rem;
-        height: 51rem;
-        margin-top: 0;
-    `}
-`
+  ${respondTo.lg`
+    height: 400px;
+    width: 60%;
+  `}
 
-const ButtonBox = styled(Box3)`
-    display: felx;
-    justify-content: center;
-
-    ${respondTo.lg`
-        justify-content: flex-start;
-    `}
-`
+  ${respondTo.xl`
+    height: 600px;
+    width: 50%;
+  `}
+`;
