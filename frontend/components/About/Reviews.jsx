4c24f87935c88bbf33c96d7b0734fe4c45ecec0f
ECWireless <Elliott@valtechcreative.com>
2021-12-21 17:13:04 -0700

    Add Reviews and Story section to About

---
+++ frontend/components/About/Reviews.jsx
@@ -0,0 +1,173 @@
+import Fade from 'react-reveal/Fade';
+import styled, { css } from 'styled-components'
+import respondTo from '../Breakpoints'
+
+import { colors } from '../theme'
+import { Box3 } from '../Boxes'
+import { Container, Flex } from '../Containers'
+import { P2, P5, P6 } from '../Typography'
+
+const Reviews = ({
+  reviewsHeading,
+  reviewsSubheading,
+  reviewsPhoto,
+  reviewsQuote1,
+  reviewsName1,
+  reviewsPosition1,
+  reviewsCompany1,
+  reviewsQuote2,
+  reviewsName2,
+  reviewsPosition2,
+  reviewsCompany2,
+  reviewsQuote3,
+  reviewsName3,
+  reviewsPosition3,
+  reviewsCompany3,
+}) => {
+  return (
+    <Container>
+      <ReviewsBackground>
+        <Flex style={{width: '100%'}} align={'center'} justify={'space-between'} respond>
+          <div>
+            <P2 color={colors.white} uppercase weight={900}>
+              {reviewsHeading}
+            </P2>
+            <Box3 marginTop={10}>
+              <P5 color={colors.white} uppercase>
+                {reviewsSubheading}
+              </P5>
+            </Box3>
+          </div>
+          <Fade bottom ssrFadeout>
+            <Flex align={'center'} justify={'flex-start'} direction={'column'}>
+              <CustomFlex style={{position: 'relative'}} align={'center'} justify={'flex-start'} direction={'column'}>
+                <Box3 marginTop={25}>
+                  <CommentBubble style={{ backgroundImage: `url(${reviewsPhoto})`}} />
+                  <P6 style={{position: 'absolute', top: '4rem', left: 0, margin: '3rem'}}>
+                    {reviewsQuote1}
+                  </P6>
+                </Box3>
+              </CustomFlex>
+              <Flex align={'flex-start'} justify={'flex-start'} direction={'column'}>
+                <Box3 marginBottom={10}>
+                  <P2 style={{fontWeight: 700}} color={colors.yellow} uppercase center>
+                    {reviewsName1}
+                  </P2>
+                </Box3>
+                <TinyFont>
+                  {reviewsPosition1}
+                </TinyFont>
+                <TinyFont>
+                  {reviewsCompany1}
+                </TinyFont>
+              </Flex>
+            </Flex>
+          </Fade>
+          <Fade delay={200} bottom ssrFadeout>
+            <Flex align={'center'} justify={'flex-start'} direction={'column'}>
+              <CustomFlex style={{position: 'relative'}} align={'center'} justify={'flex-start'} direction={'column'}>
+                <Box3 marginTop={25}>
+                  <CommentBubble style={{ backgroundImage: `url(${reviewsPhoto})`}} />
+                  <P6 style={{position: 'absolute', top: '4rem', left: 0, margin: '3rem'}}>
+                      {reviewsQuote2}
+                  </P6>
+                </Box3>
+              </CustomFlex>
+              <Flex align={'flex-start'} justify={'flex-start'} direction={'column'}>
+                  <Box3 marginBottom={10}>
+                    <P2 style={{fontWeight: 700}} color={colors.yellow} uppercase center>
+                      {reviewsName2}
+                    </P2>
+                  </Box3>
+                  <TinyFont>
+                    {reviewsPosition2}
+                  </TinyFont>
+                  <TinyFont>
+                    {reviewsCompany2}
+                  </TinyFont>
+              </Flex>
+            </Flex>
+          </Fade>
+          <Fade delay={400} bottom ssrFadeout>
+            <Flex align={'center'} justify={'flex-start'} direction={'column'}>
+              <CustomFlex style={{position: 'relative'}} align={'center'} justify={'flex-start'} direction={'column'}>
+                <Box3 marginTop={25}>
+                  <CommentBubble style={{ backgroundImage: `url(${reviewsPhoto})`}} />
+                  <P6 style={{position: 'absolute', top: '4rem', left: 0, margin: '3rem'}}>
+                    {reviewsQuote3}
+                  </P6>
+                </Box3>
+              </CustomFlex>
+              <Flex align={'flex-start'} justify={'flex-start'} direction={'column'}>
+                <Box3 marginBottom={10}>
+                  <P2 style={{fontWeight: 700}} color={colors.yellow} uppercase center>
+                    {reviewsName3}
+                  </P2>
+                </Box3>
+                <TinyFont>
+                  {reviewsPosition3}
+                </TinyFont>
+                <TinyFont>
+                  {reviewsCompany3}
+                </TinyFont>
+              </Flex>
+            </Flex>
+          </Fade>
+        </Flex>
+      </ReviewsBackground>
+    </Container>
+  )
+}
+
+export default Reviews
+
+const ReviewsBackground = styled.div`
+  width: 100%;
+  background: ${colors.brown};
+  padding: 50px 30px;
+
+  ${respondTo.xl`
+    padding: 50px;
+  `}
+`
+
+const CustomFlex = styled(Flex)`
+  height: 31rem;
+
+  ${respondTo.xl`
+    height: 320px;
+  `}
+`
+
+const CommentBubble = styled.div`
+  width: 250px;
+  height: 250px;
+  background-size: contain;
+  background-position: center;
+
+  ${respondTo.xs`
+    width: 250px;
+    height: 250px;
+  `}
+
+  ${respondTo.lg`
+    width: 250px;
+    height: 250px;
+  `}
+
+  ${respondTo.xl`
+    width: 280px;
+    height: 280px;
+  `}
+`
+
+const TinyFont = styled.p`
+  font-size: 1.4rem;
+  text-align: left;
+  color: ${colors.white};
+  margin: 4px 0;
+
+  ${respondTo.xl`
+    font-size: 1.8rem;
+  `}
+`
--- frontend/components/About/Story.js
+++ frontend/components/About/Story.js
@@ -2,110 +2,91 @@ import Fade from 'react-reveal/Fade';
 import styled from 'styled-components'
 import respondTo from '../Breakpoints'
 
-// Components
-import { colors } from '../theme'
 import { Box3 } from '../Boxes'
-import { Card1 } from '../Cards'
+import { Card3 } from '../Cards'
 import { Container, Flex } from '../Containers'
-import { Line } from '../Lines'
-import { H3, P4 } from '../Typography'
+import { H4, P5 } from '../Typography'
 
 const Story = ({
-    storyHeading1,
-    storyParagraph1,
-    storyParagraph2,
-    storyHeading2,
-    storyParagraph3,
-    storyParagraph4,
-    storyPhoto,
+  storyHeading,
+  storyParagraph1,
+  storyParagraph2,
+  storyPhoto,
 }) => {
-    return (
-        <Container>
-            <Box3 marginTop={50} marginBottom={50}>
-                <Fade bottom ssrFadeout>
-                    <Card1>
-                        <Flex style={{width: '100%'}} align={'center'} justify={'space-around'} respondFlip>
-                            <StoryPhoto style={{backgroundImage: `url(${storyPhoto})`}} />
-                            <Flex align={'center'} justify={'center'} direction={'column'}>
-                                <H3 style={{fontWeight: 700}} center uppercase>
-                                    {storyHeading1}
-                                </H3>
-                                <Fade ssrFadeout>
-                                    <Box3 marginTop={25} marginBottom={25}>
-                                        <Line height={5} width={100} color={colors.yellow} />
-                                    </Box3>
-                                </Fade>
-                                <Fade delay={400} bottom ssrFadeout>
-                                    <P4 center>
-                                        {storyParagraph1}
-                                    </P4>
-                                    <P4 center>
-                                        {storyParagraph2}
-                                    </P4>
-                                </Fade>
-                                <Box3 marginTop={50}>
-                                    <H3 style={{fontWeight: 700}} center uppercase>
-                                        {storyHeading2}
-                                    </H3>
-                                </Box3>
-                                <Fade ssrFadeout>
-                                    <Box3 marginTop={25} marginBottom={25}>
-                                        <Line height={5} width={100} color={colors.yellow} />
-                                    </Box3>
-                                </Fade>
-                                <Box3 width={500}>
-                                    <Fade delay={400} bottom ssrFadeout>
-                                        <P4>
-                                            {storyParagraph3}
-                                        </P4>
-                                    </Fade>
-                                    <Fade delay={600} bottom ssrFadeout>
-                                        <Box3 marginTop={25}>
-                                            <P4>
-                                                {storyParagraph4}
-                                            </P4>
-                                        </Box3>
-                                    </Fade>
-                                </Box3>
-                            </Flex>
-                        </Flex>
-                    </Card1>
-                </Fade>
-            </Box3>
-        </Container>
-    )
+  return (
+    <Container>
+      <Box3 marginTop={50}>
+        <Fade bottom ssrFadeout>
+          <Card3>
+            <Flex justify={'space-between'} respondFlip>
+              <StyledPhoto style={{ backgroundImage: `url(${storyPhoto})`}} />
+              <Flex align={'center'} justify={'flex-start'} direction={'column'}>
+                <StyledTextContainer>
+                  <Box3 marginBottom={25}>
+                    <H4 style={{fontWeight: 900}} uppercase>
+                      {storyHeading}
+                    </H4>
+                  </Box3>
+                  <Fade delay={200} bottom ssrFadeout>
+                    <Box3 width={450} marginBottom={25}>
+                      <P5>
+                        {storyParagraph1}
+                      </P5>
+                    </Box3>
+                  </Fade>
+                  <Fade delay={400} bottom ssrFadeout>
+                    <Box3 width={450}>
+                      <P5>
+                        {storyParagraph2}
+                      </P5>
+                    </Box3>
+                  </Fade>
+                </StyledTextContainer>
+              </Flex>
+            </Flex>
+          </Card3>
+        </Fade>
+      </Box3>
+    </Container>
+  )
 }
 
 export default Story
 
-const StoryPhoto = styled.div`
-    width: 22rem;
-    height: 22rem;
-    background-size: cover;
-    background-position: center;
-    margin-top: 5rem;
+const StyledPhoto = styled.div`
+  background-position: center;
+  background-size: cover;
+  height: 320px;
+  width: 100%;
 
-    ${respondTo.xs`
-        width: 30rem;
-        height: 30rem;
-    `}
+  ${respondTo.md`
+    height: 420px;
+  `}
 
-    ${respondTo.sm`
-        width: 50rem;
-        height: 50rem;
-    `}
+  ${respondTo.lg`
+    height: 500px;
+    width: 60%;
+  `}
+`;
 
-    ${respondTo.md`
-        width: 55rem;
-        height: 55rem;
-    `}
+const StyledTextContainer = styled.div`
+  box-sizing: border-box;
+  padding: 40px 20px;
 
-    ${respondTo.lg`
-        margin-top: 0;
-    `}
+  ${respondTo.xs`
+    padding: 30px;
+  `}
 
-    ${respondTo.xl`
-        width: 80rem;
-        height: 70rem;
-    `}
-`
+  ${respondTo.sm`
+    padding: 60px;
+  `}
+
+  ${respondTo.lg`
+    padding: 40px 60px;
+  `}
+
+  ${respondTo.xl`
+    padding: 80px;
+  `}
+`;
+
