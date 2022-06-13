dcefd4c54976af5c64c97b5ca265da3afa3f1909
ECWireless <Elliott@valtechcreative.com>
2022-06-12 18:23:16 -0600

    Add blog functionality

--- frontend/client.js
+++ frontend/client.js
@@ -1,12 +1,12 @@
 // client.js
 import sanityClient from '@sanity/client'
 
-const token = process.env.SANITY_TOKEN; // Production
-// const token = process.env.BACKEND_SANITY_TOKEN; // Staging
+// const token = process.env.SANITY_TOKEN; // Production
+const token = process.env.BACKEND_SANITY_TOKEN; // Staging
 
 export default sanityClient({
-  projectId: 'w1dauhhn', // Production
-  // projectId: 'pysf6rl6', // Staging
+  // projectId: 'w1dauhhn', // Production
+  projectId: 'pysf6rl6', // Staging
   dataset: 'production', // or the name you chose in step 1
   token,
   useCdn: false, // `false` if you want to ensure fresh data
