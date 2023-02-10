03b43ed22738480866473a1f6a27b6842037da02
ECWireless <elliott@coopallc.com>
2023-02-09 18:00:29 -0700

    Switch to staging CMS

--- frontend/client.js
+++ frontend/client.js
@@ -1,12 +1,12 @@
 // client.js
 import { createClient } from 'next-sanity'
 
-const token = process.env.SANITY_TOKEN; // Production
-// const token = process.env.BACKEND_SANITY_TOKEN; // Staging
+// const token = process.env.SANITY_TOKEN; // Production
+const token = process.env.BACKEND_SANITY_TOKEN; // Staging
 
 export default createClient({
-  projectId: 'w1dauhhn', // Production
-  // projectId: 'pysf6rl6', // Staging
+  // projectId: 'w1dauhhn', // Production
+  projectId: 'pysf6rl6', // Staging
   dataset: 'production', // or the name you chose in step 1
   token,
   useCdn: false, // `false` if you want to ensure fresh data
