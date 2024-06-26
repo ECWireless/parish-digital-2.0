829ed1df19c9dbdb7468cec43443ba32ed300f8b
ECWireless <elliott@coopallc.com>
2024-06-26 11:54:27 -0400

    Try removing hero and service photos

--- frontend/components/Home/Hero.js
+++ frontend/components/Home/Hero.js
@@ -17,9 +17,9 @@ const Hero = ({
   return (
     <WebHero>
       {/* <HeroPhoto style={{ backgroundImage: `url(${heroImage})`}} /> */}
-      <HeroVideo autoPlay loop muted playsInline>
+      {/* <HeroVideo autoPlay loop muted playsInline>
         <source src='https://arweave.net/zylbPfX-DIsUH23RiKUpmjIMp3pbUzaWmCLx4NJFo3I' type="video/mp4" />
-      </HeroVideo>
+      </HeroVideo> */}
       <HeroContainer>
         <TexContainer>
           <Fade bottom ssrFadeout>
