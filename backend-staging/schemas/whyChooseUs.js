c1700752d631ae27ae7a97a5152614b92bf1aea2
ECWireless <elliott@coopallc.com>
2024-10-31 10:52:00 -0600

    Add section 3

--- backend-staging/schemas/whyChooseUs.js
+++ backend-staging/schemas/whyChooseUs.js
@@ -99,6 +99,185 @@ export default {
             hotspot: true
         }
     },
+    {
+        name: 'testimonialsHeading1',
+        title: 'Testimonials Heading 1',
+        type: 'string'
+    },
+    {
+        name: 'testimonialsHeading2',
+        title: 'Testimonials Heading 2',
+        type: 'string'
+    },
+    {
+        name: 'testimonialsTestimonial1',
+        title: 'Testimonials Testimonial 1',
+        type: 'blockContent',
+    },
+    {
+        name: 'testimonialsLogo1',
+        title: 'Testimonials Logo 1',
+        type: 'image',
+        options: {
+            hotspot: true
+        }
+    },
+    {
+        name: 'testimonialsTestimonial2',
+        title: 'Testimonials Testimonial 2',
+        type: 'blockContent',
+    },
+    {
+        name: 'testimonialsLogo2',
+        title: 'Testimonials Logo 2',
+        type: 'image',
+        options: {
+            hotspot: true
+        }
+    },
+    {
+        name: 'testimonialsTestimonial3',
+        title: 'Testimonials Testimonial 3',
+        type: 'blockContent',
+    },
+    {
+        name: 'testimonialsLogo3',
+        title: 'Testimonials Logo 3',
+        type: 'image',
+        options: {
+            hotspot: true
+        }
+    },
+    {
+        name: 'testimonialsTestimonial4',
+        title: 'Testimonials Testimonial 4',
+        type: 'blockContent',
+    },
+    {
+        name: 'testimonialsLogo4',
+        title: 'Testimonials Logo 4',
+        type: 'image',
+        options: {
+            hotspot: true
+        }
+    },
+    {
+        name: 'testimonialsTestimonial5',
+        title: 'Testimonials Testimonial 5',
+        type: 'blockContent',
+    },
+    {
+        name: 'testimonialsLogo5',
+        title: 'Testimonials Logo 5',
+        type: 'image',
+        options: {
+            hotspot: true
+        }
+    },
+    {
+        name: 'testimonialsTestimonial6',
+        title: 'Testimonials Testimonial 6',
+        type: 'blockContent',
+    },
+    {
+        name: 'testimonialsLogo6',
+        title: 'Testimonials Logo 6',
+        type: 'image',
+        options: {
+            hotspot: true
+        }
+    },
+    {
+        name: 'testimonialsTestimonial7',
+        title: 'Testimonials Testimonial 7',
+        type: 'blockContent',
+    },
+    {
+        name: 'testimonialsLogo7',
+        title: 'Testimonials Logo 7',
+        type: 'image',
+        options: {
+            hotspot: true
+        }
+    },
+    {
+        name: 'testimonialsTestimonial8',
+        title: 'Testimonials Testimonial 8',
+        type: 'blockContent',
+    },
+    {
+        name: 'testimonialsLogo8',
+        title: 'Testimonials Logo 8',
+        type: 'image',
+        options: {
+            hotspot: true
+        }
+    },
+    {
+        name: 'testimonialsTestimonial9',
+        title: 'Testimonials Testimonial 9',
+        type: 'blockContent',
+    },
+    {
+        name: 'testimonialsLogo9',
+        title: 'Testimonials Logo 9',
+        type: 'image',
+        options: {
+            hotspot: true
+        }
+    },
+    {
+        name: 'testimonialsTestimonial10',
+        title: 'Testimonials Testimonial 10',
+        type: 'blockContent',
+    },
+    {
+        name: 'testimonialsLogo10',
+        title: 'Testimonials Logo 10',
+        type: 'image',
+        options: {
+            hotspot: true
+        }
+    },
+    {
+        name: 'testimonialsTestimonial11',
+        title: 'Testimonials Testimonial 11',
+        type: 'blockContent',
+    },
+    {
+        name: 'testimonialsLogo11',
+        title: 'Testimonials Logo 11',
+        type: 'image',
+        options: {
+            hotspot: true
+        }
+    },
+    {
+        name: 'testimonialsTestimonial12',
+        title: 'Testimonials Testimonial 12',
+        type: 'blockContent',
+    },
+    {
+        name: 'testimonialsLogo12',
+        title: 'Testimonials Logo 12',
+        type: 'image',
+        options: {
+            hotspot: true
+        }
+    },
+    {
+        name: 'testimonialsTestimonial13',
+        title: 'Testimonials Testimonial 13',
+        type: 'blockContent',
+    },
+    {
+        name: 'testimonialsLogo13',
+        title: 'Testimonials Logo 13',
+        type: 'image',
+        options: {
+            hotspot: true
+        }
+    },
   ],
 
   preview: {
--- frontend/pages/why-choose-us.jsx
+++ frontend/pages/why-choose-us.jsx
@@ -7,21 +7,26 @@ import { PortableText } from '@portabletext/react'
 
 import ContactForm from 'components/Contact/ContactForm'
 import { Container } from 'components/Containers';
-import { Box, Button, Flex, Heading, Image, HStack, Spacer, VStack } from '@chakra-ui/react';
-import { H1, H3, P4 } from 'components/Typography';
+import { Box, Button, Flex, Heading, Image, HStack, Spacer, VStack, Stack } from '@chakra-ui/react';
+import { H1, H3, H4, P4 } from 'components/Typography';
 
 const ptComponents = {
   types: {
     block: ({ value }) => {
-      return value.children.map((child) => {
-        return (
-          <P4
-            color="black"
-          >
-            {child.text}
-          </P4>
-        );
-      });
+      return (
+        <P4 color="black">
+          {value.children.map((child) => {
+            return (
+              <span
+                key={child._key}
+                style={{ fontWeight: child.marks.includes('strong') ? 700 : 400 }}
+              >
+                {child.text}
+              </span>
+            );
+          })}
+        </P4>
+      )
     },
   },
 };
@@ -38,6 +43,34 @@ const WhyChooseUs = ({
   introBrands6,
   introBrands7,
   introBrands8,
+  testimonialsHeading1,
+  testimonialsHeading2,
+  testimonialsTestimonial1,
+  testimonialsLogo1,
+  testimonialsTestimonial2,
+  testimonialsLogo2,
+  testimonialsTestimonial3,
+  testimonialsLogo3,
+  testimonialsTestimonial4,
+  testimonialsLogo4,
+  testimonialsTestimonial5,
+  testimonialsLogo5,
+  testimonialsTestimonial6,
+  testimonialsLogo6,
+  testimonialsTestimonial7,
+  testimonialsLogo7,
+  testimonialsTestimonial8,
+  testimonialsLogo8,
+  testimonialsTestimonial9,
+  testimonialsLogo9,
+  testimonialsTestimonial10,
+  testimonialsLogo10,
+  testimonialsTestimonial11,
+  testimonialsLogo11,
+  testimonialsTestimonial12,
+  testimonialsLogo12,
+  testimonialsTestimonial13,
+  testimonialsLogo13,
 }) => {
   const carouselRef = useRef(null);
 
@@ -81,8 +114,8 @@ const WhyChooseUs = ({
               <H1 color="black">{introHeading}</H1>
               <Spacer />
               <PortableText
-                value={introBody}
                 components={ptComponents}
+                value={introBody}
               />
             </VStack>
           </Flex>
@@ -103,14 +136,14 @@ const WhyChooseUs = ({
             </Button>
               <Box ref={carouselRef} maxW="100%" overflowX="hidden" w="100%">
                 <Flex gap="20px" >
-                  <Image objectFit="contain" src={urlFor(introBrands1)} h="125px" minW="200px" />
-                  <Image objectFit="contain" src={urlFor(introBrands2)} h="125px" minW="200px" />
-                  <Image objectFit="contain" src={urlFor(introBrands3)} h="125px" minW="200px" />
-                  <Image objectFit="contain" src={urlFor(introBrands4)} h="125px" minW="200px" />
-                  <Image objectFit="contain" src={urlFor(introBrands5)} h="125px" minW="200px" />
-                  <Image objectFit="contain" src={urlFor(introBrands6)} h="125px" minW="200px" />
-                  <Image objectFit="contain" src={urlFor(introBrands7)} h="125px" minW="200px" />
-                  <Image objectFit="contain" src={urlFor(introBrands8)} h="125px" minW="200px" />
+                  <Image alt="brand logo 1" objectFit="contain" src={urlFor(introBrands1)} h="125px" minW="200px" />
+                  <Image alt="brand logo 2" objectFit="contain" src={urlFor(introBrands2)} h="125px" minW="200px" />
+                  <Image alt="brand logo 3" objectFit="contain" src={urlFor(introBrands3)} h="125px" minW="200px" />
+                  <Image alt="brand logo 4" objectFit="contain" src={urlFor(introBrands4)} h="125px" minW="200px" />
+                  <Image alt="brand logo 5" objectFit="contain" src={urlFor(introBrands5)} h="125px" minW="200px" />
+                  <Image alt="brand logo 6" objectFit="contain" src={urlFor(introBrands6)} h="125px" minW="200px" />
+                  <Image alt="brand logo 7" objectFit="contain" src={urlFor(introBrands7)} h="125px" minW="200px" />
+                  <Image alt="brand logo 8" objectFit="contain" src={urlFor(introBrands8)} h="125px" minW="200px" />
                 </Flex>
               </Box>
             <Button onClick={scrollRight} px={{ base: 0, sm: 6 }} variant="ghost">
@@ -119,6 +152,128 @@ const WhyChooseUs = ({
               </svg>
             </Button>
           </HStack>
+        </Container>
+      </Box>
+
+      <Box bgColor="#E0E0E0" py={{ base: 16, xs: 20, md: 28 }}>
+        <Container>
+          <VStack >
+            <H4 color="black" weight={700}>{testimonialsHeading1}</H4>
+            <H4 color="black" weight={700}>{testimonialsHeading2}</H4>
+          </VStack>
+          <Box bgColor="white" border="3px solid #707070" mt={10} p={{ base: 8, sm: 20 }}>
+            <VStack
+              css={{
+                '&::-webkit-scrollbar': {
+                  width: '10px',
+                },
+                '&::-webkit-scrollbar-track': {
+                  border: '2px solid #707070',
+                  width: '10px',
+                },
+                '&::-webkit-scrollbar-thumb': {
+                  background: "#FFD110",
+                },
+              }}
+              maxH={{ base: '400px', lg: '500px' }}
+              overflowY="scroll"
+              pl={{ base: 2, sm: 8 }}
+              pr={{ base: 4, sm: 20 }}
+              spaceY={10}
+            >
+              <Stack alignItems="center" direction={{ base: 'column-reverse', lg: 'row' }} gap={{ base: 10, lg: 20 }}>
+                <PortableText
+                  components={ptComponents}
+                  value={testimonialsTestimonial1}
+                />
+                <Image h={{ base: '50px', lg: '125px' }} minW={{ base: '100px', lg: '300px' }} objectFit="contain" src={urlFor(testimonialsLogo1)} />
+              </Stack>
+              <Stack alignItems="center" direction={{ base: 'column-reverse', lg: 'row' }} gap={{ base: 10, lg: 20 }}>
+                <PortableText
+                  components={ptComponents}
+                  value={testimonialsTestimonial2}
+                />
+                <Image h={{ base: '50px', lg: '125px' }} minW={{ base: '100px', lg: '300px' }} objectFit="contain" src={urlFor(testimonialsLogo2)} />
+              </Stack>
+              <Stack alignItems="center" direction={{ base: 'column-reverse', lg: 'row' }} gap={{ base: 10, lg: 20 }}>
+                <PortableText
+                  components={ptComponents}
+                  value={testimonialsTestimonial3}
+                />
+                <Image h={{ base: '50px', lg: '125px' }} minW={{ base: '100px', lg: '300px' }} objectFit="contain" src={urlFor(testimonialsLogo3)} />
+              </Stack>
+              <Stack alignItems="center" direction={{ base: 'column-reverse', lg: 'row' }} gap={{ base: 10, lg: 20 }}>
+                <PortableText
+                  components={ptComponents}
+                  value={testimonialsTestimonial4}
+                />
+                <Image h={{ base: '50px', lg: '125px' }} minW={{ base: '100px', lg: '300px' }} objectFit="contain" src={urlFor(testimonialsLogo4)} />
+              </Stack>
+              <Stack alignItems="center" direction={{ base: 'column-reverse', lg: 'row' }} gap={{ base: 10, lg: 20 }}>
+                <PortableText
+                  components={ptComponents}
+                  value={testimonialsTestimonial5}
+                />
+                <Image h={{ base: '50px', lg: '125px' }} minW={{ base: '100px', lg: '300px' }} objectFit="contain" src={urlFor(testimonialsLogo5)} />
+              </Stack>
+              <Stack alignItems="center" direction={{ base: 'column-reverse', lg: 'row' }} gap={{ base: 10, lg: 20 }}>
+                <PortableText
+                  components={ptComponents}
+                  value={testimonialsTestimonial6}
+                />
+                <Image h={{ base: '50px', lg: '125px' }} minW={{ base: '100px', lg: '300px' }} objectFit="contain" src={urlFor(testimonialsLogo6)} />
+              </Stack>
+              <Stack alignItems="center" direction={{ base: 'column-reverse', lg: 'row' }} gap={{ base: 10, lg: 20 }}>
+                <PortableText
+                  components={ptComponents}
+                  value={testimonialsTestimonial7}
+                />
+                <Image h={{ base: '50px', lg: '125px' }} minW={{ base: '100px', lg: '300px' }} objectFit="contain" src={urlFor(testimonialsLogo7)} />
+              </Stack>
+              <Stack alignItems="center" direction={{ base: 'column-reverse', lg: 'row' }} gap={{ base: 10, lg: 20 }}>
+                <PortableText
+                  components={ptComponents}
+                  value={testimonialsTestimonial8}
+                />
+                <Image h={{ base: '50px', lg: '125px' }} minW={{ base: '100px', lg: '300px' }} objectFit="contain" src={urlFor(testimonialsLogo8)} />
+              </Stack>
+              <Stack alignItems="center" direction={{ base: 'column-reverse', lg: 'row' }} gap={{ base: 10, lg: 20 }}>
+                <PortableText
+                  components={ptComponents}
+                  value={testimonialsTestimonial9}
+                />
+                <Image h={{ base: '50px', lg: '125px' }} minW={{ base: '100px', lg: '300px' }} objectFit="contain" src={urlFor(testimonialsLogo9)} />
+              </Stack>
+              <Stack alignItems="center" direction={{ base: 'column-reverse', lg: 'row' }} gap={{ base: 10, lg: 20 }}>
+                <PortableText
+                  components={ptComponents}
+                  value={testimonialsTestimonial10}
+                />
+                <Image h={{ base: '50px', lg: '125px' }} minW={{ base: '100px', lg: '300px' }} objectFit="contain" src={urlFor(testimonialsLogo10)} />
+              </Stack>
+              <Stack alignItems="center" direction={{ base: 'column-reverse', lg: 'row' }} gap={{ base: 10, lg: 20 }}>
+                <PortableText
+                  components={ptComponents}
+                  value={testimonialsTestimonial11}
+                />
+                <Image h={{ base: '50px', lg: '125px' }} minW={{ base: '100px', lg: '300px' }} objectFit="contain" src={urlFor(testimonialsLogo11)} />
+              </Stack>
+              <Stack alignItems="center" direction={{ base: 'column-reverse', lg: 'row' }} gap={{ base: 10, lg: 20 }}>
+                <PortableText
+                  components={ptComponents}
+                  value={testimonialsTestimonial12}
+                />
+                <Image h={{ base: '50px', lg: '125px' }} minW={{ base: '100px', lg: '300px' }} objectFit="contain" src={urlFor(testimonialsLogo12)} />
+              </Stack>
+              <Stack alignItems="center" direction={{ base: 'column-reverse', lg: 'row' }} gap={{ base: 10, lg: 20 }}>
+                <PortableText
+                  components={ptComponents}
+                  value={testimonialsTestimonial13}
+                />
+                <Image h={{ base: '50px', lg: '125px' }} minW={{ base: '100px', lg: '300px' }} objectFit="contain" src={urlFor(testimonialsLogo13)} />
+              </Stack>
+            </VStack>
+          </Box>
         </Container>
       </Box>
       <ContactForm
@@ -146,6 +301,34 @@ const query = groq`*[_type == "whyChooseUs" && slug.current == "v1"][0]{
   introBrands6,
   introBrands7,
   introBrands8,
+  testimonialsHeading1,
+  testimonialsHeading2,
+  testimonialsTestimonial1,
+  testimonialsLogo1,
+  testimonialsTestimonial2,
+  testimonialsLogo2,
+  testimonialsTestimonial3,
+  testimonialsLogo3,
+  testimonialsTestimonial4,
+  testimonialsLogo4,
+  testimonialsTestimonial5,
+  testimonialsLogo5,
+  testimonialsTestimonial6,
+  testimonialsLogo6,
+  testimonialsTestimonial7,
+  testimonialsLogo7,
+  testimonialsTestimonial8,
+  testimonialsLogo8,
+  testimonialsTestimonial9,
+  testimonialsLogo9,
+  testimonialsTestimonial10,
+  testimonialsLogo10,
+  testimonialsTestimonial11,
+  testimonialsLogo11,
+  testimonialsTestimonial12,
+  testimonialsLogo12,
+  testimonialsTestimonial13,
+  testimonialsLogo13,
 }`
 
 export async function getStaticProps() {
