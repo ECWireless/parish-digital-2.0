e8ab1d1dd96536fdcc56ee85870ff4f9ab06ea3b
ECWireless <elliott@coopallc.com>
2025-03-11 15:20:02 -0600

    Test new way of adding Google Analytics

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,20 +1,19 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
-import { ServerStyleSheet } from 'styled-components'
-import Script from 'next/script'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+import Script from 'next/script';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
-        })
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+        });
 
-      const initialProps = await Document.getInitialProps(ctx)
+      const initialProps = await Document.getInitialProps(ctx);
       return {
         ...initialProps,
         styles: (
@@ -23,9 +22,9 @@ export default class MyDocument extends Document {
             {sheet.getStyleElement()}
           </>
         ),
-      }
+      };
     } finally {
-      sheet.seal()
+      sheet.seal();
     }
   }
 
@@ -33,38 +32,35 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en" suppressHydrationWarning>
         <Head>
-          <link rel="preload" href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&display=swap" as="style" />
-          <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&display=swap" rel="stylesheet" />
-          {/* Google Tag */}
-          <Script src="https://www.googletagmanager.com/gtag/js?id=G-G3H9CHRJWC" />
-          <Script dangerouslySetInnerHTML={
-            { __html: `
+          <link
+            rel="preload"
+            href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&display=swap"
+            as="style"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+
+        <body>
+          <Script
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=G-G3H9CHRJWC`}
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
 
               gtag('config', 'G-G3H9CHRJWC');
             `}
-          } />
-          <Script type="application/ld+json"
-            dangerouslySetInnerHTML={
-              {
-                __html: JSON.stringify({
-                  "@context": "https://schema.org",
-                  "@type": "Organization",
-                  "name": "Parish Digital Video Production",
-                  "url": "https://parishdigital.com",
-                  "logo": "https://parishdigital.com/pd.jpg"
-                })
-              }
-            }
-          />
-        </Head>
-        <body>
+          </Script>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
